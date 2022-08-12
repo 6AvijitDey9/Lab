9bae5406d04f99f0ac47f93269633400ee7358ff
Avijit Dey <110467299+6AvijitDey9@users.noreply.github.com>
2022-08-12 17:56:24 +0530

    Update 12th August Lab Session.java

--- 12th August Lab Session.java
+++ 12th August Lab Session.java
@@ -5,15 +5,15 @@ class Average
 public static void main(String []args)
 {
 System.out.println("The five numbers are:");
-int a=10;
+int a=10;//taking inputs
 int b=20;
 int c=30;
 int d=40;
 int e=50;
-System.out.println(a+","+b+","+c+","+d+","+e);
+System.out.println(a+","+b+","+c+","+d+","+e);//printing the inputs
 int sum = a+b+c+d+e;
 int average = sum/5;
-System.out.println("The Average of five numbers is = " +  average);
+System.out.println("The Average of five numbers is = " +  average);//printing the output
 }
 }
 
@@ -27,14 +27,14 @@ class Swap
 public static void main(String ...args)
 {
 System.out.println("The two numbers for swapping are:");
-int a=10;
+int a=10;//taking inputs
 int b=20;
 int c;
 System.out.println(a+","+b);
-c=a;
+c=a;//swapping process
 a=b;
 b=c;
-System.out.println("After swapping the values are = " + a+","+b);
+System.out.println("After swapping the values are = " + a+","+b);//print after swapping
 }
 }
 
@@ -49,7 +49,7 @@ class JavaSetup
 public static void main(String []args)
 {
 System.out.println("Java Setup is completed on your device : ");
-System.out.println("Java Version: 1.8.0_71");
+System.out.println("Java Version: 1.8.0_71");//printing out the the java version details
 System.out.println("Java Runtime Version: 1.8.0_71-b15");
 System.out.println("Java Home: /opt/jdk/jdk1.8.0_71/jre");
 System.out.println("Java Vendor: Oracle Corporation");
@@ -69,9 +69,9 @@ class Expression
 public static void main(String ...args)
 {
 System.out.println("The given specified expression is:");
-System.out.println("(25.5*3.5-3.5*3.5)/(40.5-4.5)");
-double c = (25.5*3.5-3.5*3.5)/(40.5-4.5);
-System.out.println("After calculating the result is = " + c);
+System.out.println("(25.5*3.5-3.5*3.5)/(40.5-4.5)");//printing out the expression
+double c = (25.5*3.5-3.5*3.5)/(40.5-4.5);//calculating and storing in a variable
+System.out.println("After calculating the result is = " + c);//printing the output
 }
 }
 
@@ -86,16 +86,16 @@ class Calculator
 public static void main(String []args)
 {
 System.out.println("The two numbers are:");
-int a=125;
+int a=125;//taking inputs
 int b=24;
-System.out.println(a+","+b);
-int sum = a+b;
+System.out.println(a+","+b);//printing the inputs
+int sum = a+b;//doing operations and storing it in a variable
 int substract = a-b;
 int multiply = a*b;
 double divide = a/b;
 double reminder = a%b;
 
-System.out.println("The sum is = " +  sum);
+System.out.println("The sum is = " +  sum);//printing the output of operations
 System.out.println("The substraction is = " +  substract);
 System.out.println("The multiplication is = " +  multiply);
 System.out.println("The dividation is = " +  divide);
