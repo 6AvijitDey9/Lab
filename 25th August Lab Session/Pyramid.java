892dfb8e81e9ff43a63757ed62e4b8799404e42e
Avijit Dey <110467299+6AvijitDey9@users.noreply.github.com>
2022-08-25 17:46:31 +0530

    Update Pyramid.java

--- 25th August Lab Session/Pyramid.java
+++ 25th August Lab Session/Pyramid.java
@@ -5,18 +5,18 @@ import java.util.*;
 public class Pyramid {
 
 	public static void main(String[] args) {
-		Scanner av= new Scanner(System.in);
+		Scanner av= new Scanner(System.in); // Scanner class
 		System.out.println("Enter the rows of Pyramid: ");
-		int row=av.nextInt();
+		int row=av.nextInt(); // Storing the rows of pyramid 
 		
-		for(int i=1; i<=row; i++) {
-			for(int j=row-i; j>=1; j--) {
+		for(int i=1; i<=row; i++) { // Outer loop
+			for(int j=row-i; j>=1; j--) { // First inner loop to print space
 			System.out.print(" ");	
 			}
-			for(int j=1; j<=i; j++) {
+			for(int j=1; j<=i; j++) { // Another inner loop to print *
 				System.out.print("* ");
 			}
-			System.out.println();
+			System.out.println(); // Printing new line
 		}
 
 	}
