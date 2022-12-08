297f17f1305c6c013852413969be58ece6c94708
Avijit Dey <110467299+6AvijitDey9@users.noreply.github.com>
2022-12-08 03:05:56 -0800

    Update BookStore.java

--- 17th November Lab Session/BookStore.java
+++ 17th November Lab Session/BookStore.java
@@ -1,6 +1,23 @@
+/*Design class called BookStore which contains an appropriate collection object to store Book instances.
+Implement the below operations.
+
+1. addBook(Book b)
+To add a new Book object into the collection.
+
+2. searchByTitle(String title)
+Search a book based on title and if found, display the details
+
+3. searchByAuthor(String author) Search a book based on author and if found, display the details
+
+4. displayAll()
+Print the details of all the books
+
+Store both classes in a package com.book.*/
+
+
 package com.book;
 import java.util.ArrayList;
-public class BookStore {	//main class
+public class BookStore { //Main class
 	ArrayList<Book> bookList = new ArrayList<>(); //Creating arrayList object to store book objects
 	
 	public void addBook(Book b) {	//Passing book object as argument
@@ -17,13 +34,13 @@ public class BookStore {	//main class
 				System.out.println(b);
 				break;
 			}else {
-				System.out.println("not found");
+				System.out.println("Book not found");
 				break;
 			}
 		}
 	} 
 	
-	public void searchByAuthor(String author) {	//Passing author string as argument to search
+	public void searchByAuthor(String author) { //Passing author string as argument to search
 		for (Book b : bookList) {
 			if(author.contains(b.getAuthor())) {
 				System.out.println(b);
