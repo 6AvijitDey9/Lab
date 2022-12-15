01b23e61b682223b82af458b37eb33693f792432
Avijit Dey <110467299+6AvijitDey9@users.noreply.github.com>
2022-12-15 13:27:57 -0800

    Delete Students_Admission.sql

--- 15th December Lab Session/Students_Admission.sql
+++
@@ -1,4 +0,0 @@
-use assignments;
-create table admission(Registration_Id numeric(4) primary key, name varchar (20), address varchar (20), phone_no numeric (10), dept varchar (10), 10th_marks numeric(5,2), 12th_marks numeric(5,2), year_of_passing int(4));
-create table students(Registration_Id numeric(4), password varchar(6));
-alter table students add foreign key(Registration_Id) references admission(Registration_Id);
